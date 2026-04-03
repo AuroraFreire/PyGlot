f120a6fcb5992e736ad655d1089a112cb4454fd6
LourencoFreire <lourencofreire3@gmail.com>
2026-04-03 19:03:06 +0100

    i dont remember what I did, I did the changes like 1 hour ago. Short term memory loss i real guys

--- main.py
+++ main.py
@@ -4,19 +4,16 @@ import json
 import builtins
 import keyword
 
-
 def create_list():
     all_builtins = [b for b in dir(builtins)]
     all_keywords = keyword.kwlist
     combined = all_builtins + all_keywords
     return combined
 
-
 def save_data():
     if not os.path.exists("lang_data.json"):
         with open("lang_data.json", "w") as json_file:
             json.dump(LANGUAGES, json_file)
-
 
 def search_lang(lang):
     with open("lang_data.json", "r") as json_file:
@@ -26,11 +23,6 @@ def search_lang(lang):
     else:
         print("There is not such language. Try typing again")
         return True
-
-
-def get_language(get_lang):
-    if get_lang == False:
-        return
 
 def translate(lang):
     with open("lang_data.json", "r") as json_file:
@@ -44,24 +36,24 @@ def translate(lang):
     for word in WORDS_LIST:
         try:
             translated.append(translator.translate(word))
-        except Exception as e:
+            print(translator.translate(word))
+        except Exception:
             translated.append(word)
+            print(translator.translate(word))
+    translated
+    with open("translated_data.json", "w") as json_file:
+        json.dump(translated, json_file, ensure_ascii=False)
     return translated
-
 
 LANGUAGES = gtrans().get_supported_languages(as_dict=True)
 WORDS_LIST = create_list()
-
 
 def main():
     save_data()
     lang = input("What language would you like to use? ")
     while search_lang(lang) == True:
         lang = input("What language would you like to use? ")
-    get_lang = search_lang(lang)
-    search_lang(lang)
-    get_language(get_lang)
-
+    translate(lang)
 
 if __name__ == "__main__":
     main()
